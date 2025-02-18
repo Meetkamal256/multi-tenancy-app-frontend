206df1ac249f85ef8141a1c7182e4f4ea4f25c56
meetkamal256 <meetkamal256@gmail.com>
2025-02-18 17:49:31 +0100

    make analytic chart responsive for tablets

--- app/_components/analyticsChart/AnalyticsChart.tsx
+++ app/_components/analyticsChart/AnalyticsChart.tsx
@@ -1,72 +1,81 @@
-                                                                                                                                                "use client";
-                                                                                                                                                import React from "react";
-                                                                                                                                                import { Line } from "react-chartjs-2";
-                                                                                                                                                import {
-                                                                                                                                                Chart as ChartJS,
-                                                                                                                                                CategoryScale,
-                                                                                                                                                LinearScale,
-                                                                                                                                                PointElement,
-                                                                                                                                                LineElement,
-                                                                                                                                                Title,
-                                                                                                                                                Tooltip,
-                                                                                                                                                Legend,
-                                                                                                                                                } from "chart.js";
-                                                                                                                                                import { tenantsData } from "../../data"
+"use client";
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+} from "chart.js";
+import { tenantsData } from "../../data";
 
-                                                                                                                                                // Register required chart components
-                                                                                                                                                ChartJS.register(
-                                                                                                                                                CategoryScale,
-                                                                                                                                                LinearScale,
-                                                                                                                                                PointElement,
-                                                                                                                                                LineElement,
-                                                                                                                                                Title,
-                                                                                                                                                Tooltip,
-                                                                                                                                                Legend
-                                                                                                                                                );
+// Register required chart components
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip
+);
 
-                                                                                                                                                const AnalyticsChart = () => {
-                                                                                                                                                // Group tenants by year of creation and calculate total data usage for each year
-                                                                                                                                                const dataByYear = tenantsData
-                                                                                                                                                    .filter((tenant) => tenant.active) // Filter only active tenants
-                                                                                                                                                    .reduce((acc, tenant) => {
-                                                                                                                                                    const year = new Date(tenant.creationDate).getFullYear();
-                                                                                                                                                    if (!acc[year]) acc[year] = 0;
-                                                                                                                                                    acc[year] += tenant.dataUsage;
-                                                                                                                                                    return acc;
-                                                                                                                                                    }, {} as { [key: number]: number });
-                                                                                                                                                
-                                                                                                                                                const years = Object.keys(dataByYear).map((year) => parseInt(year));
-                                                                                                                                                const totalDataUsage = years.map((year) => dataByYear[year]);
-                                                                                                                                                
-                                                                                                                                                // Chart data
-                                                                                                                                                const data = {
-                                                                                                                                                    labels: years,
-                                                                                                                                                    datasets: [
-                                                                                                                                                    {
-                                                                                                                                                        label: "Total Data Usage of Active Tenants",
-                                                                                                                                                        data: totalDataUsage,
-                                                                                                                                                        borderColor: "rgba(75, 192, 192, 1)",
-                                                                                                                                                        backgroundColor: "rgba(75, 192, 192, 0.2)",
-                                                                                                                                                        fill: true,
-                                                                                                                                                    },
-                                                                                                                                                    ],
-                                                                                                                                                };
-                                                                                                                                                
-                                                                                                                                                // Chart options
-                                                                                                                                                const options = {
-                                                                                                                                                    responsive: true,
-                                                                                                                                                    plugins: {
-                                                                                                                                                    legend: {
-                                                                                                                                                        position: "top",
-                                                                                                                                                    },
-                                                                                                                                                    title: {
-                                                                                                                                                        display: true,
-                                                                                                                                                        text: "Total Data Usage by Active Tenants per Year",
-                                                                                                                                                    },
-                                                                                                                                                    },
-                                                                                                                                                };
-                                                                                                                                                
-                                                                                                                                                return <Line data={data} options={options} />;
-                                                                                                                                                };
+const AnalyticsChart = () => {
+  // Group tenants by year of creation and calculate total data usage for each year
+  const dataByYear = tenantsData
+    .filter((tenant) => tenant.active) // Filter only active tenants
+    .reduce((acc, tenant) => {
+      const year = new Date(tenant.creationDate).getFullYear();
+      if (!acc[year]) acc[year] = 0;
+      acc[year] += tenant.dataUsage;
+      return acc;
+    }, {} as { [key: number]: number });
 
-                                                                                                                                                export default AnalyticsChart;
+  const years = Object.keys(dataByYear).map((year) => parseInt(year));
+  const totalDataUsage = years.map((year) => dataByYear[year]);
+
+  // Chart data
+  const data = {
+    labels: years,
+    datasets: [
+      {
+        label: "Total Data Usage of Active Tenants",
+        data: totalDataUsage,
+        borderColor: "rgba(75, 192, 192, 1)",
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        fill: true,
+      },
+    ],
+  };
+
+  // Chart options with responsiveness adjustments
+  const options = {
+    responsive: true, // Enable responsiveness
+    maintainAspectRatio: false, // Allow aspect ratio to adjust based on container size
+    plugins: {
+      title: {
+        display: true,
+        text: "Total Data Usage by Active Tenants per Year",
+      },
+    },
+    scales: {
+      x: {
+        beginAtZero: true, // Ensure the x-axis starts at 0 for consistency
+      },
+      y: {
+        beginAtZero: true, // Ensure the y-axis starts at 0
+      },
+    },
+  };
+
+  return (
+    <div style={{ position: "relative", height: "400px" }}>
+      {/* Dynamically set the height based on the screen size */}
+      <Line data={data} options={options} />
+    </div>
+  );
+};
+
+export default AnalyticsChart;
