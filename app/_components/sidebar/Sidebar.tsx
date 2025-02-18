a6e14cb7e1b7c50171b41b45c67c7026f4e4cdf9
meetkamal256 <meetkamal256@gmail.com>
2025-02-18 02:43:37 +0100

    add a menubar for toggling sidebar in smaller devices

--- app/_components/sidebar/Sidebar.tsx
+++ app/_components/sidebar/Sidebar.tsx
@@ -1,33 +1,40 @@
 "use client";
-import React from "react";
+import { useState } from "react";
 import styles from "./sidebar.module.css";
-import { FaHome, FaUsers, FaChartBar } from "react-icons/fa";
+import { FaHome, FaUsers, FaChartBar, FaBars, FaTimes } from "react-icons/fa";
+
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className={styles.sidebar}>
-      <div className={styles.logo}>
-        <h2>SaaS Admin</h2>
-      </div>
+    <>
+      <button className={styles.menuToggle} onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </button>
+      <nav className={`${styles.sidebar} ${isOpen ? styles.open : ""}`}>
+        <div className={styles.logo}>
+          <h2>SaaS Admin</h2>
+        </div>
 
-      <ul className={styles.menu}>
-        <li>
-          <a href="#" className={styles.menuItem}>
-            <FaHome className={styles.icon} /> Dashboard
-          </a>
-        </li>
-        <li>
-          <a href="#" className={styles.menuItem}>
-            <FaUsers className={styles.icon} /> Tenants
-          </a>
-        </li>
-        <li>
-          <a href="#" className={styles.menuItem}>
-            <FaChartBar className={styles.icon} /> Analytics
-          </a>
-        </li>
-      </ul>
-    </nav>
+        <ul className={styles.menu}>
+          <li>
+            <a href="#" className={styles.menuItem}>
+              <FaHome className={styles.icon} /> Dashboard
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.menuItem}>
+              <FaUsers className={styles.icon} /> Tenants
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.menuItem}>
+              <FaChartBar className={styles.icon} /> Analytics
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
